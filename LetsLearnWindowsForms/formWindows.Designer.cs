2fc037ee2caf8cd7b4451c00fd117c07dbcba0d1
ShiroNeko <103792247+Shiro-N3ko@users.noreply.github.com>
2022-09-02 12:33:48 -0400

    HelloWorld

---
+++ LetsLearnWindowsForms/formWindows.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace LetsLearnWindowsForms
+{
+    partial class formWindows
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnHelloWorld = new System.Windows.Forms.Button();
+            this.lblHelloWorld = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnHelloWorld
+            // 
+            this.btnHelloWorld.Location = new System.Drawing.Point(706, 504);
+            this.btnHelloWorld.Name = "btnHelloWorld";
+            this.btnHelloWorld.Size = new System.Drawing.Size(75, 23);
+            this.btnHelloWorld.TabIndex = 0;
+            this.btnHelloWorld.Text = "Click Me!";
+            this.btnHelloWorld.UseVisualStyleBackColor = true;
+            this.btnHelloWorld.Click += new System.EventHandler(this.btnHelloWorld_Click);
+            // 
+            // lblHelloWorld
+            // 
+            this.lblHelloWorld.AutoSize = true;
+            this.lblHelloWorld.Location = new System.Drawing.Point(695, 226);
+            this.lblHelloWorld.Name = "lblHelloWorld";
+            this.lblHelloWorld.Size = new System.Drawing.Size(97, 15);
+            this.lblHelloWorld.TabIndex = 1;
+            this.lblHelloWorld.Text = "Click The Button!";
+            // 
+            // formWindows
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1518, 629);
+            this.Controls.Add(this.lblHelloWorld);
+            this.Controls.Add(this.btnHelloWorld);
+            this.Name = "formWindows";
+            this.Text = "formWindows";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button btnHelloWorld;
+        private Label lblHelloWorld;
+    }
+}
